65b2a713e9a186c749b80fdcf7f2c051d22ae951
Manish Karki <manishkarki@USMBP.local>
2018-09-20 23:19:00 -0400

    initial commit

--- src/main/java/com/neosoft/docker/dockerspringboot/controller/DemoRestController.java
+++ src/main/java/com/neosoft/docker/dockerspringboot/controller/DemoRestController.java
@@ -1,4 +1,17 @@
 package com.neosoft.docker.dockerspringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mkarki
+ */
+
+@RestController
 public class DemoRestController {
+
+    @GetMapping("/docker/spring")
+    public String demo() {
+        return "docker spring app";
+    }
 }
